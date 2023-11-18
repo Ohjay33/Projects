eaf63b92b66b01b22488877b0f56650b660baf9c
Ohjay33 <61249083+Ohjay33@users.noreply.github.com>
2023-11-18 09:16:19 -0600

    new commit

---
+++ MyWebsite/script.js
@@ -0,0 +1,7 @@
+function over(element){
+    element.style.backgroundColor = 'yellow'
+}
+
+function out(element){
+    element.style.backgroundColor = 'lime'
+}
---
+++ Website2/script.js
@@ -0,0 +1,12 @@
+function count(obj){
+    console.log("1,2,2,2,2,")
+    console.log(obj)
+}
+
+function over(element){
+    element.style.backgroundColor = 'yellow'
+}
+
+function out(element){
+    element.style.backgroundColor = 'lime'
+}
